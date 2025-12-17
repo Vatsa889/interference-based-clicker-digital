20475e0b76e5b9291043e4543207a7b2bc816dd9
Vatsa Kumar <83350384+Vatsa889@users.noreply.github.com>
2025-12-17 01:02:36 +0100

    Update prototype_clicker.gd
    DISCLAMER: there was some AI use within the code, I wanted to disclose that just to clear up any possible suspicions in the future. The use of AI is less than 30% for the coding part for the entirety of this project

--- prototype_clicker.gd
+++ prototype_clicker.gd
@@ -5,6 +5,10 @@ extends Control
 
 @export var label : Label # made a reference to label button by highlighting label : and going to the root node and then clicking on label
 
+@export var upgrade_label : Label # Link this to your new upgrade button label
+var click_value : int = 1 # Tracks how strong your click is (starts at 1)
+var upgrade_cost : int = 10 # Tracks how much the upgrade costs
+
 var stardust : int = 0 #current amount of stardust created
 
 
@@ -23,3 +27,19 @@ func update_label_text() -> void:
 ## triggered when the create stardust button is pressed
 func _on_button_pressed() -> void:
 	create_stardust()
+
+## updates the text of the upgrade label
+func update_upgrade_label() -> void:
+	if upgrade_label: #quick check to see if it is assigned
+		upgrade_label.text = "Upgrade (Cost: %s)" %upgrade_cost
+
+##triggered when the new upgrade button is pressed
+func _on_upgrade_button_pressed() -> void:
+	if stardust >= upgrade_cost: # check if we can afford it
+		stardust -= upgrade_cost # pay the cost
+		click_value += 1 # increase click strength
+		upgrade_cost *= 2 # make next one more expensive 
+		
+		## refresh both labels
+		update_label_text()
+		update_upgrade_label()
