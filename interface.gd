d4ee5a706674b96b5e572387d3da8edd3a7beefb
Vatsa Kumar <83350384+Vatsa889@users.noreply.github.com>
2025-12-19 00:44:56 +0100

    Create interface.gd
    smth is broken idk what
    someone pls help me

---
+++ interface.gd
@@ -0,0 +1,11 @@
+class_name UserInterface
+extends Control
+
+
+enum Views {
+	PROTOTYPE_GENERATOR,
+	PROTOTYPE_CLICKER,
+}
+
+signal navigation_requested(view : Views)
+## WHY IS IT BVROEKEENNENENENENENENNENE ISTG I WILL START CRYINGGGGGGGG
