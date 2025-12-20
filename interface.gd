9730ed119640c1afc29e137dcd4c8dfb4f016ccc
Vatsa Kumar <83350384+Vatsa889@users.noreply.github.com>
2025-12-20 01:02:55 +0100

    Update interface.gd

--- interface.gd
+++ interface.gd
@@ -1,11 +1,24 @@
 class_name UserInterface
 extends Control
+## main class controlling the user interface
 
 
+##list of views
 enum Views {
 	PROTOTYPE_GENERATOR,
 	PROTOTYPE_CLICKER,
 }
 
+
+## emitted when something requested navigation, includes the view target
 signal navigation_requested(view : Views)
-## WHY IS IT BVROEKEENNENENENENENENNENE ISTG I WILL START CRYINGGGGGGGG
+## I fixed it, I am happy now
+
+
+## triggered when the prototype generator link is clicked
+func _on_prototypegeneratorlink_pressed() -> void:
+	navigation_requested.emit(Views.PROTOTYPE_GENERATOR)
+
+## triggered when the prototype clicker link is clicked
+func _on_prototypeclickerlink_pressed() -> void:
+	navigation_requested.emit(Views.PROTOTYPE_CLICKER)
