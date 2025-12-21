ddc6c86813c0225e603371cc5989d04e63112cac
Vatsa Kumar <83350384+Vatsa889@users.noreply.github.com>
2025-12-22 00:29:52 +0100

    Update stardust_label.gd

--- stardust_label.gd
+++ stardust_label.gd
@@ -3,10 +3,21 @@ extends Label
 ##displays the current amount of stardust available
 
 
-## temp function to update the text
-func _process(_delta: float) -> void:
+## connectying signaless
+func _ready() -> void:
 	update_text()
+	
+	HandlerStardust.ref.stardust_created.connect(update_text)
+	HandlerStardust.ref.stardust_consumed.connect(update_text)
 
-
-func update_text() -> void:
+# there's the _quantity arguement there to match the signal
+# the default is set to -1 so I have to manually review it without any agruements
+func update_text(_quantity : int = -1) -> void:
 	text = "stardust : %s" %Game.ref.data.stardust
+## guess what
+## what
+## chicken butt
+## you got me there
+## BAHAHAHAHHAHA
+## what
+## wait will the reviewer guy see this
