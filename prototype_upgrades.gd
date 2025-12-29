730790493d3d39dd7328c35bd24e9b091a85122d
Vatsa Kumar <83350384+Vatsa889@users.noreply.github.com>
2025-12-29 06:42:57 +0100

    Update prototype_upgrades.gd

--- prototype_upgrades.gd
+++ prototype_upgrades.gd
@@ -1,15 +1,23 @@
 class_name PrototypeUpgrades
-extends Control
+extends View
 
-@export var user_interface : UserInterface
-@export var view : UserInterface.Views
-
-func _ready() -> void:
-	visible = false
-	user_interface.navigation_requested.connect(_on_navigation_request)
+@onready var container : VBoxContainer = $MarginContainer/VBoxContainer
 
 func _on_navigation_request(requested_view : UserInterface.Views) -> void:
 	if requested_view == view:
 		visible = true
 		return
 	visible = false
+
+func _ready() -> void:
+	super._ready() # Call parent setup
+	
+	# Example upgrade spawning
+	add_upgrade(Up01ClickerUpgrade.new())
+
+func add_upgrade(upgrade_data) -> void:
+	var new_compo = COMPO_SCENE.instantiate()
+	container.add_child(new_compo)
+	new_compo.setup(upgrade_data)
+
+const COMPO_SCENE = preload("res://scenes/prototypes/upgrades/components/compo_upgrade.tscn")
